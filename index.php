41245e177f61b3576e210cfd06848ba56ce3f90f
MhaviPL <matheuslp330@gmail.com>
2026-02-20 12:16:59 -0300

    criando o recebimento da rota

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php 
 
-header("Content-Type: application/json; charset=UTF-8")
+header("Content-Type: application/json; charset=UTF-8");
 
+//Descobrir qual rota foi chamada
+$url = $_GET['url'] ?? '';
+$rota = explode('/', filter_var(rtrim($url, '/'), FILTER_SANITIZE_URL));
 
+    //EXEMPLO GET /users/id
+
+$recurso = $rota[0] ?? ''; //   /users
+$id = $rota[1] ?? ''; //   /id
+
+$metodo = $_SERVER['REQUEST_METHOD'];
 
 ?>
